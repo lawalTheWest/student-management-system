a574f70fcc5c40ed50145a19bf2e699864948b24
The West <45957199+lawalogunsola63@users.noreply.github.com>
2022-12-17 14:05:04 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,21 @@
 #include <ctype.h>
+#include <windows.h>
+#include <studio.h>
+#include <stdlib.h>
+
+void loading (){
+   printf("\n"");
+
+   int num;
+   for(num = 1; num <= 5; num++){
+      Print("\t*");
+      sleep(500);
+   }
+
+ 
+}
+
+int main ()
+{
+
+}
